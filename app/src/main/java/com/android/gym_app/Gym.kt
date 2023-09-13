e2f81008952511eda954bb57f0c8a1a1e7617ed4
Abdelrahman Samy <abdelrahmansamy123>
2023-09-13 22:52:51 +0300

    implement mvvm (view model) + used state handling in compose + hosting in compose + recover from system killing our app

--- app/src/main/java/com/android/gym_app/Gym.kt
+++ app/src/main/java/com/android/gym_app/Gym.kt
@@ -2,53 +2,22 @@ package com.android.gym_app
 
 val listOfGyms = listOf<Gym>(
     Gym(
-        "Sporting Gym",
+        1, "Sporting Gym",
         "20 Al Khasos Public Street, Al-Khasos, Qalyubia Governorate 32168496, Egypt"
     ),
     Gym(
-        "Golden Gym",
+        2, "Golden Gym",
         "32 Al Khasos Public Street, Al-Khasos, Qalyubia Governorate 66498798, Egypt"
     ),
     Gym(
-        "Falcon Gym",
+        3, "Falcon Gym",
         "85 Al Khasos Public Street, Al-Khasos, Qalyubia Governorate 54544858, Egypt"
     ),
     Gym(
-        "Active Gym",
-        "100 Al Khasos Public Street, Al-Khasos, Qalyubia Governorate 7487478, Egypt"
-    ),
-    Gym(
-        "Sporting Gym",
-        "20 Al Khasos Public Street, Al-Khasos, Qalyubia Governorate 32168496, Egypt"
-    ),
-    Gym(
-        "Golden Gym",
-        "32 Al Khasos Public Street, Al-Khasos, Qalyubia Governorate 66498798, Egypt"
-    ),
-    Gym(
-        "Falcon Gym",
-        "85 Al Khasos Public Street, Al-Khasos, Qalyubia Governorate 54544858, Egypt"
-    ),
-    Gym(
-        "Active Gym",
-        "100 Al Khasos Public Street, Al-Khasos, Qalyubia Governorate 7487478, Egypt"
-    ),
-    Gym(
-        "Sporting Gym",
-        "20 Al Khasos Public Street, Al-Khasos, Qalyubia Governorate 32168496, Egypt"
-    ),
-    Gym(
-        "Golden Gym",
-        "32 Al Khasos Public Street, Al-Khasos, Qalyubia Governorate 66498798, Egypt"
-    ),
-    Gym(
-        "Falcon Gym",
-        "85 Al Khasos Public Street, Al-Khasos, Qalyubia Governorate 54544858, Egypt"
-    ),
-    Gym(
-        "Active Gym",
+        4, "Active Gym",
         "100 Al Khasos Public Street, Al-Khasos, Qalyubia Governorate 7487478, Egypt"
     )
+
 )
 
-data class Gym(val name: String, val place: String)
+data class Gym(val id: Int, val name: String, val place: String, var isFavourite: Boolean = false)
--- app/src/main/java/com/android/gym_app/GymsScreen.kt
+++ app/src/main/java/com/android/gym_app/GymsScreen.kt
@@ -1,6 +1,7 @@
 package com.android.gym_app
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -8,6 +9,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Place
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -18,26 +21,54 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.android.gym_app.ui.theme.Gym_AppTheme
 
 @Composable
 fun GymsScreen() {
+    val vm: GymsViewModel = viewModel()
     LazyColumn() {
-        items(listOfGyms) {
-            GymItem(it)
+        items(vm.state) { gym ->
+            GymItem(gym) {
+                vm.toggleFavouriteState(it)
+            }
         }
     }
 }
 
 @Composable
-fun GymItem(gym: Gym) {
+fun GymItem(gym: Gym, onClick: (Int) -> Unit) {
+    val icon = if (gym.isFavourite) {
+        Icons.Filled.Favorite
+    } else {
+        Icons.Filled.FavoriteBorder
+    }
     Card(elevation = 8.dp, modifier = Modifier.padding(8.dp)) {
         Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(8.dp)) {
-            GymIcon(Icons.Filled.Place, Modifier.weight(0.15f))
-            GymDetails(gym, Modifier.weight(0.85f))
+            DefaultIcon(Icons.Filled.Place, Modifier.weight(0.15f), "Location Icon")
+            GymDetails(gym, Modifier.weight(0.70f))
+            DefaultIcon(icon, Modifier.weight(0.15f), "Favourite Gym Icon") {
+                onClick(gym.id)
+            }
         }
-
     }
+}
+
+@Composable
+fun DefaultIcon(
+    icon: ImageVector,
+    modifier: Modifier,
+    contentDescriptor: String,
+    onClick: () -> Unit = {},
+) {
+    Image(
+        imageVector = icon,
+        contentDescription = contentDescriptor,
+        modifier = modifier
+            .padding(8.dp)
+            .clickable { onClick() },
+        colorFilter = ColorFilter.tint(Color.DarkGray)
+    )
 }
 
 @Composable
@@ -57,17 +88,6 @@ fun GymDetails(gym: Gym, modifier: Modifier) {
             )
         }
     }
-}
-
-@Composable
-fun GymIcon(vector: ImageVector, modifier: Modifier) {
-    Image(
-        imageVector = vector,
-        contentDescription = "Gym Icon",
-        modifier = modifier,
-        colorFilter = ColorFilter.tint(Color.DarkGray)
-    )
-
 }
 
 
